15e0cafdb5b4fc1f57ab710aa0aeffeac8b7cbb8
isaac <>
2026-04-21 21:10:33 +0400

    Remove deprecated MTInputStream methods

--- submodules/MtProtoKit/Sources/MTInputStream.m
+++ submodules/MtProtoKit/Sources/MTInputStream.m
@@ -96,27 +96,6 @@ static inline int roundUpInput(int numToRound, int multiple)
     return value;
 }
 
-- (int64_t)readInt64
-{
-    int64_t value = 0;
-    
-    if ([_wrappedInputStream read:(uint8_t *)&value maxLength:8] != 8)
-    {
-        if (MTLogEnabled()) {
-            MTLog(@"***** Couldn't read int64");
-        }
-    }
-    
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-#elif __BYTE_ORDER == __BIG_ENDIAN
-#   error "Big endian is not implemented"
-#else
-#   error "Unknown byte order"
-#endif
-    
-    return value;
-}
-
 - (int64_t)readInt64:(bool *)failed
 {
     int64_t value = 0;
@@ -178,20 +157,6 @@ static inline int roundUpInput(int numToRound, int multiple)
     return value;
 }
 
-- (NSData *)readData:(int)length
-{
-    uint8_t *bytes = (uint8_t *)malloc(length);
-    NSInteger readLen = [_wrappedInputStream read:bytes maxLength:length];
-    if (readLen != length)
-    {
-        if (MTLogEnabled()) {
-            MTLog(@"***** Couldn't read %d bytes", length);
-        }
-    }
-    NSData *data = [[NSData alloc] initWithBytesNoCopy:bytes length:length freeWhenDone:true];
-    return data;
-}
-
 - (NSData *)readData:(int)length failed:(bool *)failed
 {
     uint8_t *bytes = (uint8_t *)malloc(length);
@@ -203,20 +168,6 @@ static inline int roundUpInput(int numToRound, int multiple)
         return nil;
     }
     NSData *data = [[NSData alloc] initWithBytesNoCopy:bytes length:length freeWhenDone:true];
-    return data;
-}
-
-- (NSMutableData *)readMutableData:(NSUInteger)length
-{
-    uint8_t *bytes = (uint8_t *)malloc(length);
-    NSInteger readLen = [_wrappedInputStream read:bytes maxLength:length];
-    if (readLen != length)
-    {
-        if (MTLogEnabled()) {
-            MTLog(@"***** Couldn't read %d bytes", length);
-        }
-    }
-    NSMutableData *data = [[NSMutableData alloc] initWithBytesNoCopy:bytes length:length freeWhenDone:true];
     return data;
 }
 
@@ -232,60 +183,6 @@ static inline int roundUpInput(int numToRound, int multiple)
     }
     NSMutableData *data = [[NSMutableData alloc] initWithBytesNoCopy:bytes length:length freeWhenDone:true];
     return data;
-}
-
-- (NSString *)readString
-{
-    uint8_t tmp = 0;
-    [_wrappedInputStream read:&tmp maxLength:1];
-    
-    int paddingBytes = 0;
-    
-    int32_t length = tmp;
-    if (length == 254)
-    {
-        length = 0;
-        [_wrappedInputStream read:((uint8_t *)&length) + 1 maxLength:3];
-        length >>= 8;
-        
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-#elif __BYTE_ORDER == __BIG_ENDIAN
-#   error "Big endian is not implemented"
-#else
-#   error "Unknown byte order"
-#endif
-        
-        paddingBytes = roundUpInput(length, 4) - length;
-    }
-    else
-    {
-        paddingBytes = roundUpInput(length + 1, 4) - (length + 1);
-    }
-    
-    NSString *string = nil;
-    
-    if (length > 0)
-    {
-        uint8_t *bytes = (uint8_t *)malloc(length);
-        NSInteger readLen = [_wrappedInputStream read:bytes maxLength:length];
-        if (readLen != length)
-        {
-            if (MTLogEnabled()) {
-                MTLog(@"***** Couldn't read %d bytes", length);
-            }
-        }
-        
-        string = [[NSString alloc] initWithBytesNoCopy:bytes length:length encoding:NSUTF8StringEncoding freeWhenDone:true];
-    }
-    else
-    {
-        string = @"";
-    }
-    
-    for (int i = 0; i < paddingBytes; i++)
-        [_wrappedInputStream read:&tmp maxLength:1];
-    
-    return string;
 }
 
 - (NSString *)readString:(bool *)failed
@@ -340,44 +237,6 @@ static inline int roundUpInput(int numToRound, int multiple)
         [_wrappedInputStream read:&tmp maxLength:1];
     
     return string;
-}
-
-- (NSData *)readBytes
-{
-    uint8_t tmp = 0;
-    [_wrappedInputStream read:&tmp maxLength:1];
-    
-    int paddingBytes = 0;
-    
-    int32_t length = tmp;
-    if (length == 254)
-    {
-        length = 0;
-        [_wrappedInputStream read:((uint8_t *)&length) + 1 maxLength:3];
-        length >>= 8;
-        
-        paddingBytes = roundUpInput(length, 4) - length;
-    }
-    else
-    {
-        paddingBytes = roundUpInput(length + 1, 4) - (length + 1);
-    }
-    
-    uint8_t *bytes = (uint8_t *)malloc(length);
-    NSInteger readLen = [_wrappedInputStream read:bytes maxLength:length];
-    if (readLen != length)
-    {
-        if (MTLogEnabled()) {
-            MTLog(@"***** Couldn't read %d bytes", length);
-        }
-    }
-    
-    NSData *result = [NSData dataWithBytesNoCopy:bytes length:length freeWhenDone:true];
-    
-    for (int i = 0; i < paddingBytes; i++)
-        [_wrappedInputStream read:&tmp maxLength:1];
-    
-    return result;
 }
 
 - (NSData *)readBytes:(bool *)failed
