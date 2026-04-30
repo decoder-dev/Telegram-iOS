8830c01e530b616126801cae7dfc6a02fee2005a
Isaac <>
2026-04-30 22:13:55 +0200

    Extract tgcalls helpers into tgcalls

--- tools/tgcalls_cli/group_mode.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-int runGroupMode(int customParticipants, int referenceParticipants, int duration, bool quiet, bool video, const std::string& networkScenario = "");
