306b8aad0d9932f0faa37c25f4c28b13eabf5f8c
D3C0Y <96624794+decoder-dev@users.noreply.github.com>
2026-08-03 22:40:53 +0000

    Bump SPM package platforms to iOS 15 / macOS 11

--- submodules/MosaicLayout/Package.swift
+++ submodules/MosaicLayout/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "MosaicLayout",
-    platforms: [.iOS(.v13), .macOS(.v10_13)],
+    platforms: [.iOS(.v15), .macOS(.v11)],
     products: [
         .library(name: "MosaicLayout", targets: ["MosaicLayout"]),
     ],
